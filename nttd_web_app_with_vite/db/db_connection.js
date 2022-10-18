4fc1e31c54c305d09d3aae877a914cd42411514c
Dongming Liu <dl35@illinois.edu>
2022-10-17 23:02:10 -0500

    drafting connection js

--- nttd_web_app_with_vite/db/db_connection.js
+++ nttd_web_app_with_vite/db/db_connection.js
@@ -1,4 +1,20 @@
 var mysql = require('mysql2');
+var body_parser = require('body-parser');
+var path = require('path');
+var express = require('express');
 
 // database user
-const nttd_sql_user = {};
+const nttd_sql_user = {
+        host: '35.193.104.159',
+        user: 'notimetodata:us-central1:project-411-22',
+        password: 'NoTimeToData22',
+        database: 'db'
+};
+
+var connection = mysql.createConnection(nttd_sql_user, {
+        multipleStatements: true
+});
+
+module.exports = {
+        connection
+};
