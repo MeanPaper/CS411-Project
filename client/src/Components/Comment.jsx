16d740250e077a8cf3603500c48335c921f70a24
MeanPaper <ldm1833991255@gmail.com>
2022-12-01 01:16:31 -0600

    update

--- client/src/Components/Comment.jsx
+++ client/src/Components/Comment.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 
 const Comment = ({logInStatus}) =>{
-    return (<section className='comment-section'>
+    return (<div className='comment-section'>
         <div className='comment-area'>
             <div className="comment-title">Comment:</div>
             <div className="user-comment-box" placeholder="What is your comment... "
-                contentEditable> </div>
+                contentEditable='true'> </div>
         </div>
         {/* loading all the user comments, and capped the visible comment to 10 most recent post */}
-    </section>);   
+    </div>);   
 }
 
 export default Comment;
