89468e1fc02bd8ea000efb68880e1dc45c2c29ee
MeanPaper <ldm1833991255@gmail.com>
2023-01-02 14:05:29 -0600

    sync updates

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-    return (<h2 className='homepage'>Home</h2>);
+    return (<h2 className='homepage'>Welcome To Find My Gened</h2>);
 }
 
 export default HomePage;
